2924f01e30b37a8e3c7fe3d295883980b07e2d96
Emmy168 <65239629+Emmy168@users.noreply.github.com>
2020-09-27 16:16:15 +0200

    Create herhaalboodschap.py

---
+++ herhaalboodschap.py
@@ -0,0 +1,7 @@
+def herhaalboodschap():
+    for i in range(3):
+        print("Dag Klas")
+    for i in range(500):
+        print("Gelukkig hoef ik dit niet 500x te te typen")
+
+herhaalboodschap()
