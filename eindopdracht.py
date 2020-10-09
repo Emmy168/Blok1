4c103bfff02219c24695d7132a573f1510de36fd
Emmy168 <65239629+Emmy168@users.noreply.github.com>
2020-10-09 10:20:35 +0200

    Update eindopdracht.py

--- eindopdracht.py
+++ eindopdracht.py
@@ -1,3 +1,61 @@
+###############################################################################
+# Programming Eindopdracht Blok 1
+# Date: 09/10/2020
+# Author: Emmy Schipper
+# Klas: ID1G1a
+###############################################################################
+# Dit programma checkt of de medewerker naar zijn/haar
+# werk mag komen, wegens COVID-19, of thuis moet blijven.
+# Er moet nog gewerkt worden aan de klachten lijst
+# en hoe deze uitgeprint wordt.
+################################################################################
+
+#Import modules
+import sys
+import wikipedia as wikipedia
+
+#Intro input
+naam = input("Wat is uw naam? ")
+print("Hoi",naam,"welkom bij de 'kan-ik-wel-naar-mijn-werk' COVID-19 check!"
+                 "\nHieronder wat algemene informatie over COVID-19: ")
+
+print(wikipedia.summary("Coronavirus"))
+
+#Functies
+def kwetsbaar():
+    antwoord = input("Behoort u tot de groep kwetsbare personen? JA/NEE: ")
+    if antwoord.lower() != "ja":
+        print("Gelukkig. We kunnen door naar de volgende stap.")
+    else:
+        print("Blijf thuis en bel uw werkgever.")
+        sys.exit()
+
+def ggd():
+    antwoord = input("Bent u door de GGD gebeld over contact met iemand met COVID-19? JA/NEE: ")
+    if antwoord.lower() != "ja":
+        print("Mooi zo. We kunnen door naar de volgende stap.")
+    else:
+        print("Blijf thuis en bel uw werkgever.")
+        sys.exit()
+
+def temperatuur(check):
+    geen_koorts = 37.5
+    wel_koorts = 37.6
+    if check <= geen_koorts:
+        print("U heeft geen verhoging.")
+    elif check >= wel_koorts:
+        print("Uw lichaamstemperatuur is te hoog! Blijf thuis en bel GGD voor een corona test.")
+        sys.exit()
+
+#Refers naar functies en user input
+kwetsbaar()
+ggd()
+
+#Refers naar functie temperatuur
+temp = float(input("Wat is uw lichaamstemperatuur? "))
+temperatuur(temp)
+
+
 #Lijst van klachten
 klachten_lijst = []
 
